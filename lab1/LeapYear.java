481c74cccdd0950c6d1ceb532148159d71a6e8a8
aviatesk <aviatesk@gmail.com>
2018-12-28 06:14:03 +0900

    simplify LeapYear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -4,16 +4,12 @@
 public class LeapYear {
 
     public static boolean isLeapYear(int year) {
-        if (year % 4 != 0) {
-            return false;
-        } else if (year % 100 == 0) {
-            if (year % 400 == 0) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
+        if (year % 400 == 0) {
             return true;
+        } else if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        } else {
+            return false;
         }
     }
 
