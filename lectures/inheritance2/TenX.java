60dfba59efcddbb37cc10b1e930728854b4358da
aviatesk <aviatesk@gmail.com>
2018-12-28 11:58:53 +0900

    move every lecture code to other repository

--- lectures/inheritance2/TenX.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by hug on 2/6/2017.
- */
-public class TenX implements IntUnaryFunction {
-    /** Returns ten times the argument. */
-    public int apply(int x) {
-        return 10 * x;
-    }
-}
