4d74680c088889ba718257f0c10524c1d8e4b1f3
aviatesk <aviatesk@gmail.com>
2019-02-02 00:18:03 +0900

    Project 1B: Modify TestOffByOne.java so that it covers all special cases

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -9,20 +9,36 @@ public class TestOffByOne {
 
     // Your tests go here.
     @Test
-    public void testEqualsCharTrue() {
+    public void testEqualsCharLower() {
+        assertFalse(offByOne.equalChars('a', 'a'));
         assertTrue(offByOne.equalChars('a', 'b'));
         assertTrue(offByOne.equalChars('b', 'a'));
-        assertTrue(offByOne.equalChars('A', 'B'));
-        assertTrue(offByOne.equalChars('&', '%'));
+        assertFalse(offByOne.equalChars('a', 'c'));
+        assertFalse(offByOne.equalChars('c', 'a'));
     }
 
     @Test
-    public void testEqualsCharFalse() {
-        assertFalse(offByOne.equalChars('a', 'e'));
-        assertFalse(offByOne.equalChars('z', 'a'));
+    public void testEqualsCharUpper() {
+        assertFalse(offByOne.equalChars('A', 'A'));
+        assertTrue(offByOne.equalChars('A', 'B'));
+        assertTrue(offByOne.equalChars('B', 'A'));
+        assertFalse(offByOne.equalChars('A', 'C'));
+        assertFalse(offByOne.equalChars('C', 'A'));
+    }
+
+    @Test
+    public void testEqualsCharNonAlphabet() {
+        assertTrue(offByOne.equalChars('%', '&'));
+        assertTrue(offByOne.equalChars('&', '%'));
+        assertFalse(offByOne.equalChars('$', '&'));
+        assertFalse(offByOne.equalChars('&', '$'));
+    }
+
+    @Test
+    public void testEqualsCharMixed() {
         assertFalse(offByOne.equalChars('a', 'A'));
-        assertFalse(offByOne.equalChars('A', 'Z'));
-        assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('a', '%'));
+        assertFalse(offByOne.equalChars('A', '%'));
     }
 
 }
